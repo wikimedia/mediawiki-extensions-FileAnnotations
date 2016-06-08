bf5dc0c6a26623881d49eced814c3dfe343e5b03
Mark Holmquist <mtraceur@member.fsf.org>
2016-06-08 11:13:23 -0500

    Adding initial files.
    Change-Id: Ida670547976e554381d1eb0196d90b005d0b2920

---
+++ FileAnnotator.hooks.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Hooks for FileAnnotator extension
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+class FileAnnotatorHooks {
+	public static function onRegistration() {
+		return true;
+	}
+
+	public static function onSetup() {
+		return true;
+	}
+
+	public static function getModulesForFilePage( &$out, &$skin ) {
+		if ( $out->getTitle()->inNamespace( NS_FILE ) ) {
+			$out->addModules( array( 'fileannotator' ) );
+		}
+	}
+
+	public static function onParserSetup( Parser $parser ) {
+		$parser->setHook( 'annotation', 'FileAnnotatorHooks::renderAnnotation' );
+		$parser->setHook( 'annotatedfile', 'FileAnnotatorHooks::renderAnnotatedFile' );
+		return true;
+	}
+
+	public static function renderAnnotatedFile( $input, array $args, Parser $parser, PPFrame $frame ) {
+		$output = '<div class="annotated-file">' .
+			$parser->recursiveTagParse( $input, $frame ) .
+			'</div>';
+
+		return $output;
+	}
+
+	public static function renderAnnotation( $input, array $args, Parser $parser, PPFrame $frame ) {
+		$output = '<div class="file-annotation" style="display:none"';
+
+		if ( isset( $args['x'] ) ) {
+			$output .= ' data-x="' .
+				htmlspecialchars( $args['x'] ) .
+				'"';
+		}
+
+		if ( isset( $args['y'] ) ) {
+			$output .= ' data-y="' .
+				htmlspecialchars( $args['y'] ) .
+				'"';
+		}
+
+		if ( isset( $args['w'] ) ) {
+			$output .= ' data-w="' .
+				htmlspecialchars( $args['w'] ) .
+				'"';
+		}
+
+		if ( isset( $args['h'] ) ) {
+			$output .= ' data-h="' .
+				htmlspecialchars( $args['h'] ) .
+				'"';
+		}
+
+		$output .= '>'
+			.
+			$parser->recursiveTagParse( $input, $frame ) .
+			'</div>';
+
+		return $output;
+	}
+}
---
+++ resources/src/fileannotator.js
@@ -0,0 +1,83 @@
+( function ( $, mw ) {
+	var api = new mw.Api(),
+		pageTitle = mw.Title.newFromText( mw.config.get( 'wgPageName' ) ),
+		isFilePage = pageTitle.getNamespaceId() === 6;
+
+	$( '.annotated-file' ).each( function () {
+		var imgTitle, imageInfoPromise, offset, imgh, imgw,
+			$annotations = $( this ),
+			$annotationWrapper = $( '<div>' ).addClass( 'annotation-wrapper' ),
+			$img = $annotations.find( 'img' );
+
+		if ( !$img || $img.length < 1 && isFilePage ) {
+			// Assume we're annotating the file whose page we're on.
+			$img = $( '#file img' );
+		}
+
+		if ( !$img || $img.length < 1 ) {
+			// No image found, what are we supposed to annotate?
+			// The irony that this is called "file annotations" when all we handle
+			// are images is not lost on me. XXX TODO HACK
+			return;
+		}
+
+		offset = $img.offset();
+		imgw = $img.width();
+		imgh = $img.height();
+		imgTitle = mw.Title.newFromImg( $img );
+
+		$annotationWrapper.css( {
+			top: offset.top,
+			left: offset.left,
+			height: imgh,
+			width: imgw
+		} );
+
+		$( 'body' ).append( $annotationWrapper );
+
+		imageInfoPromise = api.get( {
+			action: 'query',
+			prop: 'imageinfo',
+			indexpageids: true,
+			titles: imgTitle.getPrefixedDb(),
+			iiprop: 'size'
+		} );
+
+		$annotations.find( '.file-annotation' ).each( function ( i, annotation ) {
+			var $annotation = $( annotation ),
+				$annotationBox = $( '<div>' ).addClass( 'annotation-box' ),
+				height = $annotation.attr( 'data-h' ),
+				width = $annotation.attr( 'data-w' ),
+				x = $annotation.attr( 'data-x' ),
+				y = $annotation.attr( 'data-y' );
+
+			imageInfoPromise.done( function ( data ) {
+				var ii = data.query.pages[data.query.pageids[0]].imageinfo[0],
+					fw = ii.width,
+					fh = ii.height,
+					adjustRatioX = imgw / fw,
+					adjustRatioY = imgh / fh,
+					adjustedX = x * adjustRatioX,
+					adjustedWidth = width * adjustRatioX,
+					adjustedY = y * adjustRatioY,
+					adjustedHeight = height * adjustRatioY;
+
+				$annotationBox.css( {
+					top: adjustedY,
+					left: adjustedX,
+					height: adjustedHeight,
+					width: adjustedWidth
+				} );
+
+				$annotationBox.append( $annotation );
+				$annotation.removeAttr( 'style' );
+				$annotation.css( {
+					top: adjustedHeight - 10,
+					left: adjustedWidth - 10
+				} );
+
+				$annotationWrapper.append( $annotationBox );
+			} );
+		} );
+	} );
+}( jQuery, mediaWiki ) );
