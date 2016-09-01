40c71d55b3320edc07d8bc538d621b24c641d122
Mark Holmquist <mholmquist@wikimedia.org>
2016-09-01 16:51:01 -0500

    Quick code style improvements
    I sorta threw everything into one function, this is a little nicer
    to read and work with.
    
    Change-Id: If682d809ceaffa5b638a8d7e4f3714b78fd07347

--- ApiFileAnnotations.php
+++ ApiFileAnnotations.php
@@ -47,9 +47,6 @@ class ApiFileAnnotations extends ApiQueryBase {
 					$title
 				);
 
-				$parser = new Parser();
-				$popts = ParserOptions::newFromUser( $this->getUser() );
-
 				$page = WikiPage::factory( $faTitle );
 				$content = $page->getContent();
 				if ( !empty( $content ) ) {
@@ -59,233 +56,313 @@ class ApiFileAnnotations extends ApiQueryBase {
 					$annotations = $data->annotations;
 				}
 
+				$parser = new Parser();
+				$popts = ParserOptions::newFromUser( $this->getUser() );
+
 				$annotationsData = [];
 
 				if ( !empty( $annotations ) ) {
 					foreach ( $annotations as $annotation ) {
-						$text = $annotation->content;
-						$annotationData = [
-							'text' => $text
-						];
-
-						foreach ( $annotation as $key => $val ) {
-							if ( $key === 'content' ) {
-								continue;
-							}
-
-							$annotationData[$key] = $val;
-						}
-
-						if ( $shouldParse ) {
-							$presult = $parser->parse( $text, $faTitle, $popts );
-							$annotationData['parsed'] = $presult->mText;
-
-							// Check to see if we can return a special display for this annotation.
-							$dom = new DOMDocument();
-							$domFragment = $dom->createDocumentFragment();
-						   	$domFragment->appendXml( $presult->mText );
-
-							// The first element will always be a paragraph. Get its first child.
-							$possibleLink = $domFragment->firstChild->firstChild;
-
-							// Check if it's a link element.
-							if ( $possibleLink->nodeType === XML_ELEMENT_NODE && $possibleLink->nodeName === 'a' ) {
-								// Find out if the link is something we care about.
-								$href = $possibleLink->attributes->getNamedItem( 'href' )->value;
-
-								$commonsMatches = [];
-								$commonsCategoryMatch = preg_match(
-									'%^https?://commons.wikimedia.org.*(Category:.*)%',
-									$href,
-									$commonsMatches
-								);
-
-								$wpMatches = [];
-								$wpArticleMatch = preg_match(
-									'%^(https?://.*.wikipedia.org)/wiki/(.*)%',
-									$href,
-									$wpMatches
-								);
-
-								$wdMatches = [];
-								$wdEntityMatch = preg_match(
-									'%https?://(www\.)?wikidata.org/.*(Q\d+)%',
-									$href,
-									$wdMatches
-								);
-
-								if ( $commonsCategoryMatch === 1 ) {
-									$categoryName = $commonsMatches[1];
-
-									$imagesApiDataStr = file_get_contents(
-										'https://commons.wikimedia.org/w/api.php?' .
-										'action=query&prop=imageinfo&generator=categorymembers' .
-										'&gcmtype=file&gcmtitle=' .
-										urlencode( $categoryName ) .
-										'&gcmlimit=5&iiprop=url&iiurlwidth=100' .
-										'&iiurlheight=100&format=json'
-									);
-
-									$imagesApiData = json_decode( $imagesApiDataStr, true );
-
-									$pages = $imagesApiData['query']['pages'];
-
-									$imagesHtml = '<div class="category-members">';
-
-									foreach ( $pages as $id => $page ) {
-										$info = $page['imageinfo'][0];
-										$imagesHtml .=
-											'<a class="category-member" href="' . $info['descriptionurl'] . '">' .
-												'<img src="' . $info['thumburl'] . '" />' .
-											'</a>';
-									}
-
-									$imagesHtml .= '</div>';
-
-									$annotationData['parsed'] =
-										'<div class="commons-category-annotation">' .
-											$imagesHtml .
-											'<a href="' . $href . '">' .
-												'See more images' .
-											'</a>' .
-										'</div>';
-								}
-
-								if ( $wpArticleMatch === 1 ) {
-									$articleName = $wpMatches[2];
-
-									$articleApiDataStr = file_get_contents(
-										$wpMatches[1] .
-										'/w/api.php?action=query' .
-										'&titles=' . urlencode( $articleName ) .
-										'&prop=pageimages|extracts' .
-										'&piprop=thumbnail|name' .
-										'&pithumbsize=250' .
-										'&exsentences=4' .
-										'&format=json'
-									);
-
-									$articleApiData = json_decode( $articleApiDataStr, true );
-
-									$pages = $articleApiData['query']['pages'];
-
-									foreach ( $pages as $id => $page ) {
-										// There's only one page, so just do it here
-										$annotationData['parsed'] =
-											'<div class="wikipedia-article-annotation">' .
-												$page['extract'] .
-												'<p class="pageimage">' .
-													'<img src="' .
-														$page['thumbnail']['source'] .
-														'" width="' .
-														$page['thumbnail']['width'] .
-														'" height="' .
-														$page['thumbnail']['height'] .
-													'" />' .
-												'</p>' .
-											'</div>';
-									}
-								}
-
-								if ( $wdEntityMatch === 1 ) {
-									$entityId = $wdMatches[2];
-									$currentLang = $this->getLanguage()->getCode();
-
-									$entityApiDataStr = file_get_contents(
-										'https://www.wikidata.org/w/api.php' .
-										'?action=wbgetentities' .
-										'&ids=' . $entityId .
-										'&languages=en|' . $currentLang .
-										'&props=labels|descriptions|claims' .
-										'&format=json'
-									);
-
-									$entityApiData = json_decode( $entityApiDataStr, true );
-
-									$entity = $entityApiData['entities'][$entityId];
-
-									$labels = $entity['labels'];
-									$descriptions = $entity['descriptions'];
-									$claims = $entity['claims'];
-
-									$annotationData['parsed'] = '<div class="wikidata-entity-annotation">';
-
-									foreach ( $claims as $claimid => $claim ) {
-										switch ( $claimid ) {
-											case 'P18':
-												// Main image. Fetch imageinfo and render.
-												$imageApiDataStr = file_get_contents(
-													'https://commons.wikimedia.org/w/api.php' .
-													'?action=query&prop=imageinfo' .
-													'&titles=File:' . urlencode( $claim[0]['mainsnak']['datavalue']['value'] ) .
-													'&iiprop=url&iiurlwidth=200' .
-													'&iiurlheight=200&format=json'
-												);
-
-												$imageApiData = json_decode( $imageApiDataStr, true );
-
-												$pages = $imageApiData['query']['pages'];
-
-												$annotationData['parsed'] .= '<div class="wikidata-image">';
-
-												foreach ( $pages as $id => $page ) {
-													// There's only one page. Add HTML here.
-													$info = $page['imageinfo'][0];
-													$annotationData['parsed'] .=
-														'<a class="commons-image" href="' . $info['descriptionurl'] . '">' .
-															'<img src="' . $info['thumburl'] . '" />' .
-														'</a>';
-													break;
-												}
-
-												$annotationData['parsed'] .= '</div>';
-
-											default:
-												continue;
-										}
-									}
-
-									$annotationData['parsed'] .= '<div class="text-content">';
-
-									if ( isset( $labels[$currentLang] ) ) {
-										$annotationData['parsed'] .=
-											'<h2 class="wikidata-label">' .
-												$labels[$currentLang]['value'] .
-											'</h2>';
-									} elseif ( isset( $labels['en'] ) ) {
-										// Blatantly strange fallback, but we don't want to have
-										// no label...hopefully this works for 99% of things.
-										$annotationData['parsed'] .=
-											'<h2 class="wikidata-label">' .
-												$labels['en']['value'] .
-											'</h2>';
-									}
-
-									if ( isset( $descriptions[$currentLang] ) ) {
-										$annotationData['parsed'] .=
-											'<p class="wikidata-description">' .
-												$descriptions[$currentLang]['value'] .
-											'</p>';
-									} elseif ( isset( $descriptions['en'] ) ) {
-										$annotationData['parsed'] .=
-											'<p class="wikidata-description">' .
-												$descriptions['en']['value'] .
-											'</p>';
-									}
-
-									$annotationData['parsed'] .= '</div>';
-
-									$annotationData['parsed'] .= '</div>';
-								}
-							}
-						}
-						$annotationsData[] = $annotationData;
+						$annotationsData[] = $this->getAnnotationData(
+							$shouldParse,
+							$faTitle,
+							$annotation,
+							$parser,
+							$popts
+						);
 					}
 				}
 
 				$this->addPageSubItem( $pageIds[NS_FILE][$title], $annotationsData );
 			}
 		}
+	}
+
+	protected function renderCommonsAnnotation( $commonsMatches, $commonsCategoryMatch ) {
+		$categoryName = $commonsMatches[1];
+
+		$imagesApiDataStr = file_get_contents(
+			'https://commons.wikimedia.org/w/api.php' .
+			'?action=query' .
+			'&prop=imageinfo' .
+			'&generator=categorymembers' .
+			'&gcmtype=file' .
+			'&gcmtitle=' . urlencode( $categoryName ) .
+			'&gcmlimit=5' .
+			'&iiprop=url' .
+			'&iiurlwidth=100' .
+			'&iiurlheight=100' .
+			'&format=json'
+		);
+
+		$imagesApiData = json_decode( $imagesApiDataStr, true );
+
+		$pages = $imagesApiData['query']['pages'];
+
+		$imagesHtml = '<div class="category-members">';
+
+		foreach ( $pages as $id => $page ) {
+			$info = $page['imageinfo'][0];
+			$href = $info['descriptionurl'];
+			$src = $info['thumburl'];
+
+			$imagesHtml .=
+				'<a class="category-member" href="' . $href . '">' .
+					'<img src="' . $src . '" />' .
+				'</a>';
+		}
+
+		$imagesHtml .= '</div>';
+
+		return
+			'<div class="commons-category-annotation">' .
+				$imagesHtml .
+				'<a href="' . $href . '">' .
+					'See more images' .
+				'</a>' .
+			'</div>';
+	}
+
+	protected function renderWikipediaAnnotation( $wpMatches, $wpArticleMatch ) {
+		$articleName = $wpMatches[2];
+
+		$articleApiDataStr = file_get_contents(
+			$wpMatches[1] .
+			'/w/api.php?action=query' .
+			'&titles=' . urlencode( $articleName ) .
+			'&prop=pageimages|extracts' .
+			'&piprop=thumbnail|name' .
+			'&pithumbsize=250' .
+			'&exsentences=4' .
+			'&format=json'
+		);
+
+		$articleApiData = json_decode( $articleApiDataStr, true );
+
+		$pages = $articleApiData['query']['pages'];
+
+		foreach ( $pages as $id => $page ) {
+			// There's only one page, so just do it here
+			return
+				'<div class="wikipedia-article-annotation">' .
+					$page['extract'] .
+					'<p class="pageimage">' .
+						'<img src="' .
+							$page['thumbnail']['source'] .
+							'" width="' .
+							$page['thumbnail']['width'] .
+							'" height="' .
+							$page['thumbnail']['height'] .
+						'" />' .
+					'</p>' .
+				'</div>';
+		}
+	}
+
+	protected function renderWikidataAnnotation( $wdMatches, $wdEntityMatch ) {
+		$entityId = $wdMatches[2];
+		$currentLang = $this->getLanguage()->getCode();
+
+		$entityApiDataStr = file_get_contents(
+			'https://www.wikidata.org/w/api.php' .
+			'?action=wbgetentities' .
+			'&ids=' . $entityId .
+			'&languages=en|' . $currentLang .
+			'&props=labels|descriptions|claims' .
+			'&format=json'
+		);
+
+		$entityApiData = json_decode( $entityApiDataStr, true );
+
+		$entity = $entityApiData['entities'][$entityId];
+
+		$labels = $entity['labels'];
+		$descriptions = $entity['descriptions'];
+		$claims = $entity['claims'];
+
+		$imageHtml = null;
+
+		foreach ( $claims as $claimid => $claim ) {
+			switch ( $claimid ) {
+				case 'P18':
+					// Main image. Fetch imageinfo and render.
+					$imageHtml = $this->renderWdImage(
+						$claim[0]['mainsnak']['datavalue']['value']
+					);
+					break;
+
+				default:
+					continue;
+			}
+		}
+
+		$label = null;
+		$description = null;
+
+		if ( isset( $labels[$currentLang] ) ) {
+			$label =
+				'<h2 class="wikidata-label">' .
+					$labels[$currentLang]['value'] .
+				'</h2>';
+		} elseif ( isset( $labels['en'] ) ) {
+			// Blatantly strange fallback, but we don't want to have
+			// no label...hopefully this works for 99% of things.
+			$label =
+				'<h2 class="wikidata-label">' .
+					$labels['en']['value'] .
+				'</h2>';
+		}
+
+		if ( isset( $descriptions[$currentLang] ) ) {
+			$description =
+				'<p class="wikidata-description">' .
+					$descriptions[$currentLang]['value'] .
+				'</p>';
+		} elseif ( isset( $descriptions['en'] ) ) {
+			$description =
+				'<p class="wikidata-description">' .
+					$descriptions['en']['value'] .
+				'</p>';
+		}
+
+		$html = '<div class="wikidata-entity-annotation">';
+
+		if ( !is_null( $imageHtml ) ) {
+			$html .= $imageHtml;
+		}
+
+		if ( !is_null( $label ) || !is_null( $description ) ) {
+			$html .= '<div class="text-content">';
+
+			if ( !is_null( $label ) ) {
+				$html .= $label;
+			}
+
+			if ( !is_null( $description ) ) {
+				$html .= $description;
+			}
+		}
+
+		return $html;
+	}
+
+	protected function renderWdImage( $imageTitle ) {
+		$imageApiDataStr = file_get_contents(
+			'https://commons.wikimedia.org/w/api.php' .
+			'?action=query' .
+			'&prop=imageinfo' .
+			'&titles=File:' . urlencode( $imageTitle ) .
+			'&iiprop=url' .
+			'&iiurlwidth=200' .
+			'&iiurlheight=200' .
+			'&format=json'
+		);
+
+		$imageApiData = json_decode( $imageApiDataStr, true );
+
+		$pages = $imageApiData['query']['pages'];
+		$imageLink = null;
+
+		foreach ( $pages as $id => $page ) {
+			// There's only one page. Add HTML here.
+			$info = $page['imageinfo'][0];
+			return
+				'<div class="wikidata-image">' .
+					'<a class="commons-image" href="' . $info['descriptionurl'] . '">' .
+						'<img src="' . $info['thumburl'] . '" />' .
+					'</a>' .
+				'</div>';
+		}
+	}
+
+	protected function parseAnnotation( $text, $faTitle, $parser, $popts ) {
+		$presult = $parser->parse( $text, $faTitle, $popts );
+		$parsed = $presult->mText;
+
+		// Check to see if we can return a special display for this annotation.
+		$dom = new DOMDocument();
+		$domFragment = $dom->createDocumentFragment();
+		$domFragment->appendXml( $presult->mText );
+
+		// The first element will always be a paragraph. Get its first child.
+		$possibleLink = $domFragment->firstChild->firstChild;
+
+		// Check if it's a link element.
+		if ( $possibleLink->nodeType === XML_ELEMENT_NODE && $possibleLink->nodeName === 'a' ) {
+			// Find out if the link is something we care about.
+			$href = $possibleLink->attributes->getNamedItem( 'href' )->value;
+
+			$commonsMatches = [];
+			$commonsCategoryMatch = preg_match(
+				'%^https?://commons.wikimedia.org.*(Category:.*)%',
+				$href,
+				$commonsMatches
+			);
+
+			$wpMatches = [];
+			$wpArticleMatch = preg_match(
+				'%^(https?://.*.wikipedia.org)/wiki/(.*)%',
+				$href,
+				$wpMatches
+			);
+
+			$wdMatches = [];
+			$wdEntityMatch = preg_match(
+				'%https?://(www\.)?wikidata.org/.*(Q\d+)%',
+				$href,
+				$wdMatches
+			);
+
+			if ( $commonsCategoryMatch === 1 ) {
+				$parsed = $this->renderCommonsAnnotation(
+					$commonsMatches,
+					$commonsCategoryMatch
+				);
+			}
+
+			if ( $wpArticleMatch === 1 ) {
+				$parsed = $this->renderWikipediaAnnotation(
+					$wpMatches,
+					$wpArticleMatch
+				);
+			}
+
+			if ( $wdEntityMatch === 1 ) {
+				$parsed = $this->renderWikidataAnnotation(
+					$wdMatches,
+					$wdEntityMatch
+				);
+			}
+		}
+
+		return $parsed;
+	}
+
+	protected function getAnnotationData( $shouldParse, $faTitle, $annotation, $parser, $popts ) {
+		$text = $annotation->content;
+
+		$annotationData = [
+			'text' => $text
+		];
+
+		foreach ( $annotation as $key => $val ) {
+			if ( $key === 'content' ) {
+				continue;
+			}
+
+			$annotationData[$key] = $val;
+		}
+
+		if ( $shouldParse ) {
+			$annotationData['parsed'] = $this->parseAnnotation(
+				$text,
+				$faTitle,
+				$parser,
+				$popts
+			);
+		}
+
+		return $annotationData;
 	}
 
 	public function getAllowedParams() {
