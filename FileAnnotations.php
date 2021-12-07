70162123c37a8318f89080b0e5e5fb94408d75cb
Umherirrender <umherirrender_de.wp@web.de>
2021-12-07 12:44:15 +0100

    Emit deprecation warning from php entry point
    Change-Id: Ic27a9ba398a31d02879c5ea7e53fda806d156c1f

--- FileAnnotations.php
+++ FileAnnotations.php
@@ -14,8 +14,11 @@
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'FileAnnotations' );
-
+	wfWarn(
+		'Deprecated PHP entry point used for FileAnnotations extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	return true;
 }
 
-die( 'This version of the FileAnnotations extension requires MediaWiki 1.25+.' );
+die( 'This version of the FileAnnotations extension requires MediaWiki 1.34+.' );
